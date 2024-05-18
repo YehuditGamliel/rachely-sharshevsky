efc49c4042b94f1a8839320843603aa47288828f
רחל שרשבסקי <Hand4c35@mbjcomp.org.il>
2024-05-19 01:58:40 +0300

    חללח

--- client/src/components/Eyeglasses/Eyeglasses.jsx
+++ client/src/components/Eyeglasses/Eyeglasses.jsx
@@ -3,9 +3,10 @@ import { useParams, Link, useNavigate } from "react-router-dom";
 import './Eyeglasses.css'
 import { UserContext } from "../../UserProvider";
 import IconButton from '@mui/material/IconButton';
-import Button from '@mui/material/Button';
+// import Button from '@mui/material/Button';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import logo from'../../img/logo.png'
+import  SingleEyeglassee  from '../SingleEyeglasses/SingleEyeglasses.jsx';
 
 function Eyeglasses() {
 
@@ -39,19 +40,22 @@ function Eyeglasses() {
 
   return (<>
     <div id='container'>
-      {console.log(displayEyeglasses)}
+      
       {displayEyeglasses.map((eyeglasses, index) => <div key={index} class="glasses">
-        <div><img src={logo} id="image" /></div>
-        <div id="model">{eyeglasses.model}</div><br />
+        {/* <div><img src={logo} id="image" /></div> */}
+        {/* <div id="model">{eyeglasses.model}</div><br />
         <span>{eyeglasses.price}</span>
-        {/* <span >{eyeglasses.photo}</span> */}
-        <br />
+        <span >{eyeglasses.photo}</span>
+        <br /> */}
         {/* <button onClick={() => glassesInfo(eyeglasses.model)} id="buttonInfo">למידע נוסף</button> */}
-        <IconButton color="primary" aria-label="add to shopping cart">
+        {/* <IconButton color="primary" aria-label="add to shopping cart">
           <AddShoppingCartIcon />
-        </IconButton>
+        </IconButton> */}
+        <SingleEyeglassee title={eyeglasses.model} price={eyeglasses.price}/>
 
-      </div>)}</div>
+      </div>)
+      }
+      </div>
 
   </>)
 }
--- client/src/components/SingleEyeglasses/SingleEyeglasses.jsx
+++ client/src/components/SingleEyeglasses/SingleEyeglasses.jsx
@@ -1,124 +1,116 @@
-import React, { useState, useEffect, useContext } from "react";
-import { useParams } from "react-router-dom";
-import { UserContext } from "../../UserProvider";
-import '../SingleEyeglassee/SingleEyeglassee.css'
-
-function SingleEyeglasses() {
-  const { user, setUser } = useContext(UserContext);
-  const { id, postId } = useParams()
-  const [comments, setComments] = useState([])
-  const [displayInputs, setDisplayInputs] = useState({ i3sActive: false, index: 0, action: "" })
-  const [valuesComment, setValuesComment] = useState()
-
-  useEffect(() => {
-    if (id != user.id) {
-      localStorage.clear();
-      setUser()
-      navigate('/login');
-    }
-    getComments();
-  }, [])
-
-  const getComments = () => {
-    fetch(`http://localhost:8082/comments?postId=${postId}`, {
-      method: 'GET'
-    })
-      .then(response => response.json())
-      .then((json) => setComments(json))
-  }
-  const handleChange = (event) => {
-    event.preventDefault();
-    const { name, value } = event.target;
-    setValuesComment({ ...valuesComment, [name]: value })
-  }
-
-  const checkComment = (comment) => {
-    if (comment.email == user.email) {
-      return true
-    }
-    return false
-  }
 
 
-  const addComment = () => {
-    if (displayInputs.isActive) {
-      setDisplayInputs({ isActive: false, index: 0, action: "" })
-      addCommentsInDB(postId)
-      updateCommentsNextID()
-    }
-    else {
-      getCommentsNextId()
-      setDisplayInputs({ isActive: true, index: 0, action: "add" })
-    }
-  }
 
-  const addCommentsInDB = () => {
-    fetch(`http://localhost:8082/comments`, {
-      method: 'POST',
-      body: JSON.stringify({
-        id: `${nextCommentId}`,
-        postId: postId,
-        name: valuesComment.name,
-        email: user.email,
-        body: valuesComment.body
-      }),
-    })
-      .then((response) => response.json())
-      .then((json) => { setComments([...comments, json]) });
-  }
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+// import '../SingleEyeglassee/SingleEyeglassee.css'
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
-  const deleteComment = async (commentId) => {
-    await fetch(`http://localhost:8082/comments/${commentId}`, {
-      method: 'DELETE',
-    })
-    setComments(comments.filter(comment => comment.id != commentId))
-  }
+export default function SingleEyeglasses(props) {
+  const [expanded, setExpanded] = React.useState(false);
 
-  const updateComment = (commentId) => {
-    if (displayInputs.isActive) {
-      setDisplayInputs({ isActive: false, index: 0, action: "" })
-      updateCommentInDB(commentId)
-    }
-    else {
-      setDisplayInputs({ isActive: true, index: commentId, action: "update" })
-    }
-  }
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
-  const updateCommentInDB = (commentId) => {
-    fetch(`http://localhost:8082/comments/${commentId}`, {
-      method: 'PUT',
-      body: JSON.stringify({
-        name: valuesComment.name,
-        body: valuesComment.body
-      }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        setComments(comments.map((comment, index) => {
-          if (comment.id == commentId) { return comment = json }
-          return comment
-        }))
-      })
-  }
-  return (<>
-    <button id="addComment" onClick={() => addComment()}>Add</button>
-    {displayInputs.isActive && displayInputs.action == "add" && (<div>
-      <input type="text" name="name" onChange={handleChange} placeholder="name" />
-      <input type="text" name="body" onChange={handleChange} placeholder="body" /></div>)}
-    {comments.map((comment, index) => <div key={index}>
-      <p>id:{comment.id} name:{comment.name} email:{comment.email} body:{comment.body}</p>
-      {checkComment(comment) && (<div>
-        <button id="delete" onClick={() => deleteComment(comment.id)}>delete</button>
-        <button onClick={() => updateComment(comment.id)}>update</button>
-        {displayInputs.isActive && displayInputs.action == "update" && (displayInputs.index == comment.id) && (<div>
-          <input type="text" name="name" onChange={handleChange} placeholder="name" />
-          <input type="text" name="body" onChange={handleChange} placeholder="body" /></div>)}
-      </div>)}
-    </div>
-    )}
-  </>)
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={props.title}
+        subheader={props.price}
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV7O1samPbhPQ7BdbWItMidc47gFHvQMXnqd7Sd_Vt&s"
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the mussels,
+          if you like.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton color="primary" aria-label="add to shopping cart">
+          <AddShoppingCartIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Method:</Typography>
+          <Typography paragraph>
+            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
+            aside for 10 minutes.
+          </Typography>
+          <Typography paragraph>
+            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
+            medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
+            occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
+            large plate and set aside, leaving chicken and chorizo in the pan. Add
+            pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
+            stirring often until thickened and fragrant, about 10 minutes. Add
+            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+          </Typography>
+          <Typography paragraph>
+            Add rice and stir very gently to distribute. Top with artichokes and
+            peppers, and cook without stirring, until most of the liquid is absorbed,
+            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
+            mussels, tucking them down into the rice, and cook again without
+            stirring, until mussels have opened and rice is just tender, 5 to 7
+            minutes more. (Discard any mussels that don&apos;t open.)
+          </Typography>
+          <Typography>
+            Set aside off of the heat to let rest for 10 minutes, and then serve.
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
 }
-export default SingleEyeglasses
+
